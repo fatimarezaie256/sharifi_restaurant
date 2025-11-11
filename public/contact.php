96d96d1779eb4f882737d832f45d479cf3d2e38c
fatimarezaie256 <fatimarezaie112@gmail.com>
2025-11-11 09:35:53 +0430

    The contact page ended

---
+++ public/contact.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../fontawesome-free-6.7.2-web/css/all.min.css">
+    <link rel="stylesheet" href="../src/output.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512- … " crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        #hero{
+            background-image: url('../public/images/contact.jpg');
+            background-repeat:no-repeat;
+        }
+    </style>
+</head>
+<body>
+    
+        <!-- nav  started -->
+     <nav class=' p-3 bg-black/70 fixed top-0 w-full text-white border-b-[1px] border-b-white'>
+      <div class='flex justify-between'>
+        <img src="../sharifi class diagram.png" alt="" class='h-12 w-12 rounded-full'>
+        <ul class='flex flex-row gap-20 text-[20px] items-center'>
+            <li><a href="home.php">Home</a></li>
+             <li><a href="ourmenu.php">Our menu</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+        <button class=' font-bold rounded-md px-6 text-[18px] bg-green-700 text-white '> <a href="insertAdmin.php">Login</a></button>
+      </div>
+     </nav>
+     <!-- nav ended -->
+      <div class="h-[70vh] mt-20 w-full bg-stone-50/70">
+         <h1 class="text-4xl text-green-700 p-8 font-brush text-center font-bold ">Contact Us</h1>
+     <p class="text-center text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam fuga quibusdam cumque corporis ea sapiente doloribus neque error porro, voluptates, qui explicabo minus beatae incidunt culpa similique necessitatibus repellat optio.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, odio optio eaque consequuntur itaque beatae totam, quasi culpa adipisci incidunt quisquam provident saepe magnam. Odio quod ratione eos neque obcaecati.
+     </p>
+    <div class="flex gap-10 justify-evenly mt-12">
+        <div class="text-center">
+           <div><i class="fa-solid fa-location-dot text-3xl text-green-700"></i></div>
+            <p class="font-bold mt-3 text-xl">Physical Address</p>
+            <p class=" text-base mt-2">Richmond Hill,New York 11419</p>
+        </div>
+        <div class="text-center">   
+           <div
+           ><i class="text-3xl fas fa-envelope text-green-700"></i></div>
+            <p class="font-bold mt-3 text-xl">Email Address</p>
+            <p class="text-base mt-2">example@gmail.com</p>
+        </div>
+        <div
+       
+        class="text-center">
+            <div
+                 ><i class="fa-solid fa-phone text-3xl text-green-700"></i></div>
+            <p class="font-bold mt-3 text-xl">Phone Number</p>
+            <p class=" text-base mt-2">+1800-586-824</p>
+        </div>
+          </div>
+        </div>
+          <div class="h-[110vh] w-full bg-stone-50/70">
+        <div class="h-screen w-full bg-stone-50/70 overflow-x-hidden flex ">
+            
+           <div class="flex-1  items-center overflow-x-hidden h-screen w-1/2 border-r-[1px] border-r-green-700">
+           <div
+           class="text-center p-10"><h1 class="text-4xl text-green-700 font-brush">Get In Touch</h1></div>
+           <p
+           class="text-center text-[18px] overflow-x-hidden p-14">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur cumque dolor quasi minus alias, voluptates iusto soluta at corrupti consequatur enim eligendi numquam, iure, nesciunt cupiditate fugiat consectetur commodi delectus!</p>
+             <div class="grid grid-cols-3">
+                <div class="flex hover:shadow-md  bg-white shadow w-36 px-5 py-4 rounded-md gap-5">
+                   <i class="fa-brands fa-github hover:underline text-green-700 text-[22px]"></i>
+                    <h1 class="text-nowrap font-bold">Github</h1>
+                </div>
+                <div class="flex bg-white hover:shadow-md shadow w-36 px-5 py-4 rounded-md gap-5">
+              <i class="fa-brands fa-facebook hover:underline text-green-700 text-[22px]"></i>
+                    <h1 class="text-nowrap font-bold">Facebook</h1>
+                </div>
+                <div class="flex bg-white hover:shadow-md shadow w-36 px-5 py-4 rounded-md gap-5">
+                   <i class="fa-brands fa-linkedin hover:underline text-green-700 text-[22px]"></i>
+                    <h1  class="text-nowrap font-bold">Linkedin</h1>
+                </div>
+
+             </div>
+             <div class="grid grid-cols-3 mt-11">
+                <div class="flex hover:shadow-md  bg-white shadow w-36 px-5 py-4 rounded-md gap-5">
+                    <i class="fa-brands fa-twitter hover:underline text-green-700 text-[22px]"></i>
+                    <h1 class="text-nowrap font-bold">Twitter</h1>
+                </div>
+                <div class="flex bg-white hover:shadow-md shadow w-36 px-5 py-4 rounded-md gap-5">
+                 <i class="fa-solid fa-phone hover:underline text-green-700 text-[22px]"></i>
+                    <h1 class="text-nowrap font-bold">Call now</h1>
+                </div>
+                <div class="flex bg-white hover:shadow-md shadow w-36 px-5 py-4 rounded-md gap-5">
+                    <i class="fa-brands fa-instagram hover:underline text-green-700 text-[22px]"></i>
+                    <h1  class="text-nowrap font-bold">Instagram</h1>
+                </div>
+
+             </div>
+            
+            </div>
+            <div
+            
+            class="flex-1 text-center rounded-md overflow-x-hidden">
+                           <div class="bg-white shadow-md h-[95vh] mx-auto rounded-md w-[80%]">
+                           <div class="text-center p-10"><h1 class="text-4xl text-green-700 font-brush">Contact Us</h1></div>
+               <input type="text" class="border focus:outline-green-700 border-gray-400 w-[80%] p-4 h-8 mt-6 rounded-sm" placeholder="Enter your name" />
+               <input type="text" class="border focus:outline-green-700 border-gray-400 w-[80%] p-4 h-8 mt-6 rounded-sm" placeholder="Enter your email" />
+               <input type="text" class="border focus:outline-green-700 border-gray-400 w-[80%]  p-4 h-8 mt-6 rounded-sm" placeholder="Enter your subject" />
+              <textarea class="focus:outline-green-700 border border-gray-400 mt-4 rounded-sm w-[80%] h-32 text-gray-500 p-1">Enter your Comment</textarea>
+             <a href=""><button class="bg-green-700 hover:shadow-md font-bold w-[80%] text-white h-10 mt-7 hover:cursor-pointer rounded-md">Send messege</button></a>
+               </div>
+            </div>
+            
+        </div>
+        <div class=" mx-auto w-82 rounded-md gap-5">
+       <h1 class="text-green-700 font-black text-center pt-8 ">2025 &copy; Designed By Laravel Students</h1>
+   </div></div>
+</body>
+</html>
--- public/home.php
+++ public/home.php
@@ -15,11 +15,12 @@
 <body>
     <div class="h-screen bg-center bg-conic-180  bg-cover w-full" id="hero">
     <!-- nav  started -->
-     <nav class=' p-3 bg-black text-white border-b-[1px] border-b-white'>
+     <nav class=' p-3 bg-black/40 fixed top-0 w-full text-white border-b-[1px] border-b-white'>
       <div class='flex justify-between'>
         <img src="../sharifi class diagram.png" alt="" class='h-12 w-12 rounded-full'>
         <ul class='flex flex-row gap-20 text-[20px] items-center'>
             <li><a href="home.php">Home</a></li>
+            <li><a href="ourmenu.php">Our menu</a></li>
             <li><a href="contact.php">Contact</a></li>
             <li><a href="about.php">About</a></li>
         </ul>
@@ -31,7 +32,8 @@
       <h1 class="text-8xl moto font-bold p-20" >The Delicious Way <span class="block ">to Eat <i>Healthy.</i></span></h1>
       <h1 class="text-white pl-20 text-[20px] -mt-10 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente<span class="block "> fuga eaque voluptas temporibus provident hic aliquid nemo officia  </span>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
       <button class="px-5 ml-20 mt-8 hover:cursor-pointer py-4 font-bold bg-green-700 text-white transition-colors duration-700 rounded-md hover:bg-orange-400 hover:opacity-90"><a href="insertAdmin.php">Get Started</a></button>
-    </div>
+    </div> 
+      
 
        <!-- hero ended -->
         
